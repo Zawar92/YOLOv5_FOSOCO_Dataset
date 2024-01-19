d08aeb78a7cdc942929c124592e407455ffcfe0f
zawar92 <zawar92_khan@hotmail.com>
2024-01-19 03:51:33 +0100

    Move file updated.

--- move_files.sh
+++ move_files.sh
@@ -1,7 +1,7 @@
 # Moving Large data.
 
-source_folder="./prepare_clean_data/augmentation/labels/"
-destination_folder="./yolov5/data/train/labels/"
+source_folder="./prepare_clean_data/data/train/labels"
+destination_folder="./prepare_clean_data/new_data/labels"
 
 # Create the destination folder if it doesn't exist
 # mkdir -p "$destination_folder"
@@ -13,9 +13,9 @@ for file in "$source_folder"/*; do
 done
 
 
-####
-#### Moving data with requirements.
-####
+# ####
+# #### Moving data with requirements.
+# ####
 
 # sourceFolder="./yolov5/data/valid/labels"
 # destinationFolder="./prepare_clean_data/valid_robo/labels"
@@ -41,3 +41,50 @@ done
 #             ;;
 #     esac
 # done
+
+###
+### MOVE CROPS ONLY
+###
+sourceFolder="./datasets/data/valid/labels"
+destinationFolder="./prepare_clean_data/data/labels"
+
+for file in "$sourceFolder"/*; do
+    validFile=false
+
+    case "$(basename "$file")" in
+        *_blue*|*_yellow*|*_large_orange*|*_orange*|*_unknown*)
+            validFile=true
+            ;;
+    esac
+
+    if [ "$validFile" != false ]; then
+        mv "$file" "$destinationFolder"
+        echo "Moved: $file"
+    fi
+
+    case "$(basename "$file")" in
+        *rf*)
+            mv "$file" "$destinationFolder"
+            echo "Moved: $file"
+            ;;
+    esac
+done
+
+###
+### MOVE BACKGROUND IMAGES
+###
+# sourceFolder="./prepare_clean_data/orignal_images/images"
+# destinationFolder="./prepare_clean_data/background/images"
+
+# # Enable case-insensitive matching
+# shopt -s nocaseglob
+
+# for file in "$sourceFolder"/*; do
+#     if [[ "$(basename "$file")" =~ background ]]; then
+#         mv "$file" "$destinationFolder"
+#         echo "Moved: $file"
+#     fi
+# done
+
+# # Disable case-insensitive matching (optional)
+# shopt -u nocaseglob
